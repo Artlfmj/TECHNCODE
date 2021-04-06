21a6d63583cd05cb7370745884a13c41996828b7
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2021-04-06 10:49:37 +0200

    Ajout dun send message on online

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,10 @@
 //here the event starts
+const Discord = require("discord.js")
 module.exports = client => {
     console.log(`Discord Bot ${client.user.tag} is online!`); //log when ready aka the bot usable
     client.user.setActivity(`${client.user.username}`, { type: "PLAYING"}) //first parameter, is the status, second is an object with type which can be: "PLAYING", "WATCHING", "LISTENING", "STREAMING" (where you need to add a , and then url: "https://twitch.tv/#")
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`✅ | ${client.user.tag} en ligne `)
+    .setColor("BLUE")
+    client.channels.cache.get("826865550666891282").send(embed)
 }
